4d5274ca01cacb56a1e33150f46727e42854b1ae
Callum Dunster <cdunster@users.noreply.github.com>
2017-11-28 22:49:42 +0000

    Setup project workspace.
    * Created new Eclipse project.
    * Created main.c with main function.
    * Created Doxygen configuration file.
    * Created top-level .gitignore file.

---
+++ Core/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief	The main file of the project.
+ *
+ * The file that contains the program entry point and any higher-level functionality.
+ */
+
+/**
+ * @brief	Program entry point.
+ *
+ * @return	Fault code. 0 if no faults.
+ */
+int main( void )
+{
+	return 0;
+}
